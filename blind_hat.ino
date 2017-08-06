c972b6af6456c01122916886837b808593b44ec7
TKlein2980 <Jag4326@gmail.com>
2017-08-05 17:52:46 -0700

    Add files via upload

--- blind_hat.ino
+++ blind_hat.ino
@@ -1,106 +1,66 @@
-int interval = 1000;
-
-unsigned long previousMillis = millis();
-int pulseState = LOW;
-//double CM_FEET = 30.48;
-int group1[] = {5,7,8,12,13};
-int group2[] = {4,2,7,8};
-int group3[] = {3,12,13};
-int group4[] = {3,2,7};
-int group5[] = {5,2,7,8,12};
-int wholeGroup[] = {6,2,7,8,12,13};
-int mode = 0;
-//int buttonState = digitalRead(3);
-int prevButState = 0;
-
 #include <LIDARLite.h>
 #include <Wire.h>
 //#include <Ultrasonic.h>
 
-LIDARLite myLidarLite;
-//Ultrasonic u(6,5); 
+int interval;
 
-/**
- * changes the mode if button is pressed
- */
-/*void buttonPressed(){
-  mode = (mode + 1) % 3;
-}*/
-/**
- * sets an array of pins to given state
- */
-void setState(int* arr, int state){
-  for(int i = 1; i < arr[0]; i++){
+unsigned long previousMillis = millis();
+int pulseState = LOW;
+
+int group1[4] = {7,8,12,13};
+int group2[3] = {2,7,8};
+int group3[2] = {12,13};
+int group4[2] = {2,7};
+int group5[4] = {2,7,8,12};
+int wholeGroup[5] = {2,7,8,12,13};
+
+float aScale;
+int prevButState = 0;
+float vol;
+int sigVal;
+
+const int numReadings = 10;
+int readings[numReadings];      // the readings from the analog input
+int readIndex = 0;              // the index of the current reading
+int total = 0;                  // the running total
+int average = 0;                // the average
+
+LIDARLite myLidarLite; 
+
+void setState(int arr[], int state){
+  for(int i = 0; i < 5; i++){
     digitalWrite(arr[i], state);
   }
 
   return;
 }
-/**
- * checks if x is in [a,b]
- */
-boolean inRange(double x, int a, int b){
-  return a <= x and b >= x;
-}
 
 void normalMode(double dist){
 
+  sigVal = analogRead(A0);
+  aScale = fscale(0,1023,0.5,2,sigVal,0);
+  vol = dist * aScale;
 
-   /* if(mode == 1){
-      dist *= 2.05;
+    if (vol == 0) {
+      return setState(wholeGroup, LOW);
     }
-    else if(mode == 2){
-      dist *= 5;
+    else if (0 < vol <= 50) {
+      return setState(wholeGroup, pulseState);
     }
-*/
-  int sigVal = analogRead(A0);
-  float vol = sigVal * (1.0 / 1023.0);
-
-    if(vol>0.0)
-    {
-      dist*= (2.05/vol);
+    else if (50 < vol <= 100) {
+       return setState(wholeGroup, pulseState);
     }
-    
-    if (dist < 5) {
-      digitalWrite(2, pulseState);
+    else if (100 < vol <= 160) {
+       return setState(wholeGroup, pulseState);
     }
-    if (inRange(dist, 3,4)) {
-       setState(group4, pulseState);
-    }
-    else
-    {
-      setState(group1, LOW);
-    }
-     if (inRange(dist,2,3)) {
-        setState(group2, pulseState);
+    else if (160 < vol <= 200) {
+      return setState(wholeGroup, pulseState);
     }
      else
     {
-      setState(group1, LOW);
+      return setState(wholeGroup, LOW);
     }
-    if (inRange(dist,1,2)) {
-      setState(group5, pulseState);
-    }
-     else
-    {
-      setState(group1, LOW);
-    }
-    if (dist < 1) {
-      setState(wholeGroup, pulseState);
-    }
-     else
-    {
-      setState(group1, LOW);;
-    }
-  
-  
-  if(dist < 0.75){
-    setState(group3, HIGH);
-  }
 
- /**if(dist > 75 and dist <= 100){
-     setState(group3, HIGH);
- }  */
 }
 
 void setup() {
@@ -114,39 +74,128 @@ void setup() {
   //pinMode(3, INPUT);
   Serial.begin(115200);
   myLidarLite.begin(0, true);
+  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+  readings[thisReading] = 0;
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //buttonState = digitalRead(3);
-  /*if(buttonState != prevButState){
-    buttonPressed();
-    prevButState = buttonState;
-    
-  }*/
-  
+
   unsigned int currentMillis = millis();
-  
-  Serial.println(myLidarLite.distance(false));
-  //int uDist = u.Ranging(CM);
-  //Serial.print(uDist + ":::::::::::::::::::::::");
+ 
   int lidarDistance = myLidarLite.distance(false);
-  interval = (lidarDistance); //pulses faster  a lot faster...trying to see if this is easier to feel
-  //Serial.print(lidarDistance);
 
-  if(currentMillis - previousMillis >= interval){
-    previousMillis = currentMillis;
+  // subtract the last reading:
+  total = total - readings[readIndex];
+  // read from the sensor:
+  readings[readIndex] = lidarDistance;
+  // add the reading to the total:
+  total = total + readings[readIndex];
+  // advance to the next position in the array:
+  readIndex = readIndex + 1;
 
-    if(pulseState == LOW){
-      pulseState = HIGH;
-    }
-    else {
-      pulseState = LOW;
-    }
+  // if we're at the end of the array...
+  if (readIndex >= numReadings) {
+    // ...wrap around to the beginning:
+    readIndex = 0;
   }
-  //converts the distance into meters
-  double dist = (lidarDistance ) / 100;
-  normalMode(dist);
+
+  // calculate the average:
+  average = total / numReadings;
+  delay(1);        // delay in between reads for stability
+  
+  sigVal = analogRead(A0);
+  aScale = fscale(0,1023,0.5,2,sigVal,0);
+  vol = average * aScale;
+  
+  interval = average; //pulses faster  a lot faster...trying to see if this is easier to feel
+
+  
+    if(vol == 0) 
+    {
+      pulseState == LOW;
+    }
+    else if(vol > 300) 
+    {
+      pulseState == LOW;
+      setState(wholeGroup, pulseState);
+    }
+    if(currentMillis - previousMillis >= interval) {
+      previousMillis = currentMillis;        
+        if(pulseState == LOW){
+          pulseState = HIGH;
+          setState(wholeGroup, pulseState);
+        }
+        else {
+          pulseState = LOW;
+          setState(wholeGroup, pulseState);
+        }
+    }
+    
+  
+  
+  Serial.println(vol);
 
 }
 
+
+float fscale( float originalMin, float originalMax, float newBegin, float
+newEnd, float inputValue, float curve){
+
+  float OriginalRange = 0;
+  float NewRange = 0;
+  float zeroRefCurVal = 0;
+  float normalizedCurVal = 0;
+  float rangedValue = 0;
+  boolean invFlag = 0;
+
+
+  // condition curve parameter
+  // limit range
+
+  if (curve > 10) curve = 10;
+  if (curve < -10) curve = -10;
+
+  curve = (curve * -.1) ; // - invert and scale - this seems more intuitive - postive numbers give more weight to high end on output 
+  curve = pow(10, curve); // convert linear scale into lograthimic exponent for other pow function
+
+  // Check for out of range inputValues
+  if (inputValue < originalMin) {
+    inputValue = originalMin;
+  }
+  if (inputValue > originalMax) {
+    inputValue = originalMax;
+  }
+
+  // Zero Refference the values
+  OriginalRange = originalMax - originalMin;
+
+  if (newEnd > newBegin){ 
+    NewRange = newEnd - newBegin;
+  }
+  else
+  {
+    NewRange = newBegin - newEnd; 
+    invFlag = 1;
+  }
+
+  zeroRefCurVal = inputValue - originalMin;
+  normalizedCurVal  =  zeroRefCurVal / OriginalRange;   // normalize to 0 - 1 float
+  
+
+  // Check for originalMin > originalMax  - the math for all other cases i.e. negative numbers seems to work out fine 
+  if (originalMin > originalMax ) {
+    return 0;
+  }
+
+  if (invFlag == 0){
+    rangedValue =  (pow(normalizedCurVal, curve) * NewRange) + newBegin;
+
+  }
+  else     // invert the ranges
+  {   
+    rangedValue =  newBegin - (pow(normalizedCurVal, curve) * NewRange); 
+  }
+
+  return rangedValue;
+}
